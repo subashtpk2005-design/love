5ad11f2c8ccfc2abf49973aed8f957365bebb8a2
subashtpk2005-design <subashtpk2005@gmail.com>
2026-02-21 21:57:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,81 @@
+// Floating hearts animation
+setInterval(()=>{
+  let heart=document.createElement("div");
+  heart.className="heart";
+  heart.innerHTML="❤️";
+
+  heart.style.left=Math.random()*100+"%";
+  heart.style.animationDuration=(3+Math.random()*3)+"s";
+
+  document.querySelector(".hearts").appendChild(heart);
+
+  setTimeout(()=>{
+    heart.remove();
+  },6000);
+
+},400);
+
 const quiz = [
-  {
-    question: "When did I first fall for you?",
-    answers: ["Jan 20", "Feb 12", "Jan 28", "I don't remember"],
-    correct: "Jan 28"
-  },
-  {
-    question: "My favourite color?",
-    answers: ["Red", "Blue", "Black", "Pink"],
-    correct: "Black"
-  }
+{
+question: "Nammaloda Love Ennakki start akichu❤️?",
+answers: ["Jan 20", "Feb 12", "Jan 28", "Maranthutu"],
+correct: "Jan 28"
+},
+{
+question: "Namma 1st time meet pannum pothu naan enna vangi kuduthen onakku😊",
+answers: ["Teddy", "Chocolate", "onnum vangitharla", "Maranthutu"],
+correct: "Chocolate"
+},
+{
+question: "En Thangathukku Birthdaykku Enna Gift venum?",
+answers: ["Chocolate", "Teddy", "Onnum ventam", "Jimikki"],
+correct: "Teddy"
+},
+{
+question: "Onakku pudichathu yaru😁 ?",
+answers: ["Unga Appa", "Meee", "Unga Chellam", "Unga Partner"],
+correct: "Meee"
+},
+{
+question: "Enna Marriage Pannipala?",
+answers: ["Yes", "No", "Pappom"],
+correct: "Yes"
+}
 ];
 
-let current = 0;
-let score = 0;
+let current=0;
+let score=0;
 
-function loadQuestion() {
-  document.getElementById("question").innerText =
-    quiz[current].question;
+function loadQuestion(){
+document.getElementById("question").innerText=quiz[current].question;
 
-  let answerBox = document.getElementById("answers");
-  answerBox.innerHTML = "";
+let answerBox=document.getElementById("answers");
+answerBox.innerHTML="";
 
-  quiz[current].answers.forEach(ans => {
-    let btn = document.createElement("button");
-    btn.innerText = ans;
+quiz[current].answers.forEach(ans=>{
+let btn=document.createElement("button");
+btn.innerText=ans;
 
-    btn.onclick = () => {
-      if(ans === quiz[current].correct){
-        score++;
-        document.getElementById("score").innerText = score;
-      }
-      current++;
-      if(current < quiz.length) loadQuestion();
-      else alert("Quiz finished ❤️ Score: " + score);
-    };
+btn.onclick=()=>{
+if(ans===quiz[current].correct){
+score++;
+document.getElementById("score").innerText=score;
+}
 
-    answerBox.appendChild(btn);
-  });
+current++;
+
+if(current<quiz.length) loadQuestion();
+else showResult();
+};
+
+answerBox.appendChild(btn);
+});
+}
+
+function showResult(){
+document.querySelector(".quiz-box").innerHTML=
+`<h2>❤️ I Love You ❤️</h2>
+<p>நீ தான் என் வாழ்க்கை 💕</p>`;
 }
 
 loadQuestion();
